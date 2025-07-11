05ed060b68553dcfe3320a68f1ff3cc157972ebd
FlamestarRS <FlamestarRS@gmail.com>
2025-07-11 15:06:04 -0400

    handlerReadiness

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
-	serveMux := http.NewServeMux()
+	mux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: serveMux,
+		Handler: mux,
 	}
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	server.ListenAndServe()
 
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
