851d4f73755fa3e10882956085588a861e09bf30
FlamestarRS <FlamestarRS@gmail.com>
2025-07-13 14:47:16 -0400

    postgres and sqlc integration, users table, .env and functionality in main

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/FlamestarRS/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	db             *database.Queries
+}
+
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error opening sql db: %v", err)
+	}
+	dbQueries := database.New(db)
+
 	mux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
+	}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
@@ -22,8 +45,4 @@ func main() {
 
 	server.ListenAndServe()
 
-}
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
 }
